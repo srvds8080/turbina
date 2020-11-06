0e3a10ff8ed1d459be4e96ce9b6f337e92a0be3e
Rinata Baygulova <rinulya0912@gmail.com>
2020-11-06 19:54:50 +0300

    fix: починить ошибки из консоли

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,16 +1,18 @@
 import styles from "./Article.module.css";
 
 /*contentArray это массив текстовых элементов
-*которые портируются в разметку параграфами*/
-export function Article({title, contentArray}) {
+ *которые портируются в разметку параграфами*/
+export function Article({ title, contentArray }) {
   return (
     <>
       <h2 className={styles.title}>{title}</h2>
       <div className={styles.grid}>
-        {
-          contentArray.map((paragraph) => <p key={styles.content.toString()} className={styles.content}>{paragraph}</p>)
-        }
+        {contentArray.map((paragraph) => (
+          <p key={paragraph} className={styles.content}>
+            {paragraph}
+          </p>
+        ))}
       </div>
     </>
-  )
+  );
 }
